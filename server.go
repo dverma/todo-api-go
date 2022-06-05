56e17637e9728e85dde07217fccf4e0f1480273f
Dhawal Verma <dhawalverma@icloud.com>
2022-06-04 22:22:10 -0600

    POST and abstract out error handler

--- server.go
+++ server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"sync"
 )
 
 type Todo struct {
@@ -12,13 +14,14 @@ type Todo struct {
 }
 
 type todoHandlers struct {
+	sync.Mutex
 	store map[string]Todo
 }
 
 func newTodoHandlers() *todoHandlers {
 	return &todoHandlers{
 		store: map[string]Todo{
-			"id1": {
+			"id1": Todo{
 				ID:         "id1",
 				Title:      "Learn Golang",
 				IsComplete: false,
@@ -42,23 +45,42 @@ func (h *todoHandlers) restHandlers(writer http.ResponseWriter, req *http.Reques
 	}
 }
 
-func (h *todoHandlers) post(writer http.ResponseWriter, req *http.Request) {
+func errorHandler(err error, writer http.ResponseWriter, req *http.Request) {
+	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
+		writer.Write([]byte(err.Error()))
+		return
+	}
+}
 
+func (h *todoHandlers) post(writer http.ResponseWriter, req *http.Request) {
+	bodyBytes, err := ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	errorHandler(err, writer, req)
+
+	var todo Todo
+	err = json.Unmarshal(bodyBytes, &todo)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		writer.Write([]byte(err.Error()))
+		return
+	}
+	h.Lock()
+	h.store[todo.ID] = todo
+	defer h.Unlock()
 }
 
 func (h *todoHandlers) get(writer http.ResponseWriter, req *http.Request) {
 	todos := make([]Todo, len(h.store))
+	h.Lock()
 	i := 0
 	for _, todo := range h.store {
 		todos[i] = todo
 		i++
 	}
+	h.Unlock()
 	jsonBytes, err := json.Marshal(todos)
-	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
-		writer.Write([]byte(err.Error()))
-		panic(err)
-	}
+	errorHandler(err, writer, req)
 	writer.Header().Add("content-type", "application/json")
 	writer.WriteHeader(http.StatusOK)
 	writer.Write(jsonBytes)
